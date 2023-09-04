3fcdc52ed626390ae1f859554ee6904299e120af
Brandon Ramirez <darkrahd@gmail.com>
2023-09-03 23:29:02 -0600

    Reduced code duplication

--- src/features/cart/context/cartReducer.ts
+++ src/features/cart/context/cartReducer.ts
@@ -10,12 +10,6 @@ export const initialState: CartState = {
     totalPrice: 0,
 };
 
-type AddMealAction = { type: "ADD_MEAL"; payload: CartMeal };
-type RemoveMealAction = { type: "REMOVE_MEAL"; payload: string };
-type ClearCartAction = { type: "CLEAR_CART" };
-
-type CartActions = AddMealAction | RemoveMealAction | ClearCartAction;
-
 const calculateTotal = (meals: CartMeal[]) => meals.reduce((total, meal) => total + (meal.price * meal.quantity), 0);
 
 
@@ -23,34 +17,41 @@ type MealAdder = (meals: CartMeal[], newMeal: CartMeal) => CartMeal[];
 type MealRemover = (meals: CartMeal[], id: string) => CartMeal[];
 
 const addMeal: MealAdder = (meals, newMeal) => [...meals, newMeal];
-
 const removeMeal: MealRemover = (meals, mealId) => meals.filter(meal => meal.id !== mealId);
 
-const updateMeal: MealAdder = (meals, newMeal) => meals.map(meal =>
-    meal.id === newMeal.id ?
-        { ...meal, quantity: meal.quantity + newMeal.quantity } :
-        meal
-)
+/* TODO 
+The following formulas are very similar and can be abstracted, but Typescript is making it hard:
+- addOrUpdateMeal / removeOrUpdateMeal
+*/
 
-const updateMeal2: MealRemover = (meals, mealId) => meals.map(meal =>
-    meal.id === mealId ?
-        { ...meal, quantity: meal.quantity - 1 } :
+type MealUpdater = (meals: CartMeal[], newMeal: CartMeal, change: number) => CartMeal[];
+
+const updateMealQuantity: MealUpdater = (meals, newMeal, change) => meals.map(meal =>
+    meal.id === newMeal.id ?
+        { ...meal, quantity: meal.quantity + change } :
         meal
-)
+);
 
 const addOrUpdateMeal: MealAdder = (meals, newMeal) => {
     const isMealInCart = meals.some(meal => meal.id === newMeal.id);
     return isMealInCart ?
-        updateMeal(meals, newMeal) :
+        updateMealQuantity(meals, newMeal, newMeal.quantity) :
         addMeal(meals, newMeal);
 }
 
 const removeOrUpdateMeal: MealRemover = (meals, mealId) => {
-    const hasMoreThanOne = meals.find(meal => meal.id === mealId)!.quantity > 1;
+    const meal = meals.find(meal => meal.id === mealId)!
+    const hasMoreThanOne = meal.quantity > 1;
     return hasMoreThanOne ?
-        updateMeal2(meals, mealId) :
+        updateMealQuantity(meals, meal, -1) :
         removeMeal(meals, mealId);
 }
+
+type AddMealAction = { type: "ADD_MEAL"; payload: CartMeal };
+type RemoveMealAction = { type: "REMOVE_MEAL"; payload: string };
+type ClearCartAction = { type: "CLEAR_CART" };
+
+type CartActions = AddMealAction | RemoveMealAction | ClearCartAction;
 
 const cartReducer = (state: CartState, action: CartActions) => {
     switch (action.type) {
@@ -60,8 +61,6 @@ const cartReducer = (state: CartState, action: CartActions) => {
             return { meals: newMeals, totalPrice: newTotal }
         }
         case "REMOVE_MEAL": {
-            //This is currently wrong. The idea is to decrease 'quantity' by 1 if there are multiple of that meal
-            //Of remove the meal if there is only 1 before the substraction.
             const newMeals = removeOrUpdateMeal(state.meals, action.payload)
             const newTotal = calculateTotal(newMeals);
             return { meals: newMeals, totalPrice: newTotal }
