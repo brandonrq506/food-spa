b3ce4f5f354b4cd78de9c70001c80c1fabd1e8c6
Brandon Ramirez <darkrahd@gmail.com>
2023-09-03 23:53:49 -0600

    Update cart types

--- src/features/cart/types/index.ts
+++ src/features/cart/types/index.ts
@@ -2,9 +2,13 @@ import { Meal } from "../../../types";
 
 export type CartMeal = Meal & { quantity: number };
 
-export interface CartContextProps {
+export type CartState = {
     meals: CartMeal[];
+    totalItems: number;
     totalPrice: number;
+};
+
+export type CartContextProps = CartState & {
     addMeal: (meal: CartMeal) => void;
     removeMeal: (id: string) => void;
     clearCart: () => void;
